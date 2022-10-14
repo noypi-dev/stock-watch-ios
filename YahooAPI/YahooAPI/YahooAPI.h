1b5339d1c25d3c20d093842a8e449da62502dacb
Mark Anthony Degamo <degamo.markanthony@gmail.com>
2022-10-14 18:55:02 +0800

    Setup Project

---
+++ YahooAPI/YahooAPI/YahooAPI.h
@@ -0,0 +1,18 @@
+//
+//  YahooAPI.h
+//  YahooAPI
+//
+//  Created by Mark Anthony Degamo on 10/14/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for YahooAPI.
+FOUNDATION_EXPORT double YahooAPIVersionNumber;
+
+//! Project version string for YahooAPI.
+FOUNDATION_EXPORT const unsigned char YahooAPIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YahooAPI/PublicHeader.h>
+
+
